d4f2a3901c1f81e51af62e03bf77fb415f3fcc6f
captain-rudra <rudrabanna45@gmail.com>
2026-03-27 21:06:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API Running 🚀");
-});
+ 
 
 
 app.use("/api/admin", adminRoutes);
@@ -31,6 +29,10 @@ app.use("/uploads", express.static("uploads"));
 app.use("/api/products", productRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes); 
+
+app.get("/", (req, res) => {
+  res.send("API Running 🚀");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
